219a2a948e122f4ddcd6af8851fd132f735c85a1
Jeziel Lago <jeziellago@gmail.com>
2021-07-22 20:21:06 -0300

    solution

--- src/main/kotlin/FileWatcher.kt
+++ src/main/kotlin/FileWatcher.kt
@@ -1,2 +1,50 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
+import kotlinx.coroutines.launch
+import java.io.File
+import java.nio.file.*
+
 class FileWatcher {
+
+    val channel = Channel<String>(BUFFERED)
+
+    fun start(scope: CoroutineScope) {
+
+        val filePath = File("files","sample.txt")
+        val watchedDirPath: Path = filePath.parentFile.toPath()
+        val watchService: WatchService = FileSystems.getDefault().newWatchService()
+
+        watchedDirPath.register(watchService,
+            StandardWatchEventKinds.ENTRY_CREATE,
+            StandardWatchEventKinds.ENTRY_MODIFY,
+            StandardWatchEventKinds.ENTRY_DELETE
+        )
+        scope.launch {
+            while (true) {
+                channel.send("\nWatching...")
+
+                val monitorKey: WatchKey = watchService.take()
+
+                val dirPath = monitorKey.watchable() as? Path ?: break
+
+                monitorKey.pollEvents().forEach { watchEvent ->
+                    val eventPath = dirPath.resolve(watchEvent.context() as Path)
+
+                    if (eventPath.toFile().absolutePath != filePath.absolutePath) {
+                        return@forEach
+                    }
+
+                    if (watchEvent.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+                        channel.send("Modified: ${eventPath.fileName}")
+                    }
+
+                    monitorKey.reset()
+                }
+            }
+        }
+        println("daleew")
+
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,36 +1,22 @@
-import java.io.File
-import java.nio.file.FileSystems
-import java.nio.file.Path
-import java.nio.file.StandardWatchEventKinds.*
-import java.nio.file.WatchKey
-import java.nio.file.WatchService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.runBlocking
 
 fun main() {
+    val fileWatcher = FileWatcher()
 
-    val filePath = File("files","sample.txt")
-    val watchedDirPath: Path = filePath.parentFile.toPath()
-    val watchService: WatchService = FileSystems.getDefault().newWatchService()
 
-    watchedDirPath.register(watchService, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE)
+    runBlocking(Dispatchers.IO) {
 
-    while (true) {
-        println("\nWatching...")
+        fileWatcher.start(this)
 
-        val monitorKey: WatchKey = watchService.take()
-        val dirPath = monitorKey.watchable() as? Path ?: break
-
-        monitorKey.pollEvents().forEach { watchEvent ->
-            val eventPath = dirPath.resolve(watchEvent.context() as Path)
-
-            if(eventPath.toFile().absolutePath != filePath.absolutePath) {
-                return@forEach
-            }
-
-            if (watchEvent.kind() == ENTRY_MODIFY) {
-                println("Modified: ${eventPath.fileName}")
-            }
-
-            monitorKey.reset()
+        fileWatcher.channel.consumeEach { value ->
+            println(value)
         }
     }
+
 }
